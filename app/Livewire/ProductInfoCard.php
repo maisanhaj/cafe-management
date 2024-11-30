d56a84ee5150be72e736d2d9079fb7887d1bef7c
Emad Rashdan <emadiga@gmail.com>
2024-11-30 15:18:13 +0300

    Create UI for product card

--- app/Livewire/ProductInfoCard.php
+++ app/Livewire/ProductInfoCard.php
@@ -6,35 +6,42 @@ use App\Models\Product;
 use Livewire\Component;
 use Livewire\Attributes\On;
 
-class ProductInfoCard extends Component {
+class ProductInfoCard extends Component
+{
+    public int $quantity = 1;
 
-    public $quantity = 1;
-    public $showModal = false;
+    public bool $showModal = false;
 
-    public function incrementQuantity() {
+    public Product $product;
+
+    public function incrementQuantity(): void
+    {
         $this->quantity++;
     }
 
-    public function decrementQuantity() {
-        if ( $this->quantity > 1 ) {
+    public function decrementQuantity(): void
+    {
+        if ($this->quantity > 1) {
             $this->quantity--;
         }
     }
-    #[ On( 'productDisplayed' ) ]
 
-    public function showProductInfo( $productId ) {
-        $this->product = Product::findOrFail( $productId );
+    #[On('productDisplayed')]
+    public function showProductInfo($productId): void
+    {
+        $this->product = Product::findOrFail($productId);
         // $this->product = Product::select( 'description' )
         // ->findOrFail( $productId );
         $this->showModal = true;
     }
 
-    public function closeModal() {
+    public function closeModal(): void
+    {
         $this->showModal = false;
-
     }
 
-    public function render() {
-        return view( 'livewire.product-info-card' );
+    public function render()
+    {
+        return view('livewire.product-info-card');
     }
 }
